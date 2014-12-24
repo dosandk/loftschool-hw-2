61a383a0bd5b2d4516a4a4975d72e4fc82c28fb7
Vladimir Shevchuk <dosandkv@gmail.com>
2014-12-24 23:36:50 +0200

    add html markup; implemented base jade structure for page;

---
+++ app/php-core/config.local.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('DIR', 'loftschool/app/');
+    define('DOMAIN_URL', 'http://localhost/loftschool/app/');
+    define('DOCROOT', $_SERVER['DOCUMENT_ROOT']);
+    define('TEMPLATES', DOCROOT.DIR.'/core/views/templates/');
+
+
+
---
+++ app/php-core/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('DIR', '/');
+    define('DOMAIN_URL', 'http://vl-shevchuk.ru/');
+    define('DOCROOT', $_SERVER['DOCUMENT_ROOT']);
+    define('TEMPLATES', $_SERVER['DOCUMENT_ROOT'].'/core/views/templates/');
+
+
+
