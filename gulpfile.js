5dd1ce5675a0abed8c40acd4114fd708ed56524d
Vladimir Shevchuk <dosandkv@gmail.com>
2015-01-04 21:48:45 +0200

    add gulp tasks for css minification and images optimization;

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,17 @@ var gulp = require('gulp'),
     prefix = require('gulp-autoprefixer'),
     livereload = require('gulp-livereload'),
     connect = require('gulp-connect'),
+    mainBowerFiles = require('main-bower-files'),
+    gulpFilter = require('gulp-filter'),
+    imgOptimization = require('gulp-image-optimization'),
+    imagemin = require('gulp-imagemin'),
+    optipng = require('gulp-optipng'),
+    cache = require('gulp-cache'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    size = require('gulp-size'),
+    plumber = require('gulp-plumber'),
+    uncss = require('gulp-uncss'),
     compass = require('gulp-compass');
 
 gulp.task('compass', function() {
@@ -17,21 +28,46 @@ gulp.task('compass', function() {
         .pipe(gulp.dest('./dev/css'));
 });
 
-gulp.task('concat', ['compass'], function() {
-    return gulp.src(['./dev/css/*.css'])
-        .pipe(concat('ui.min.css'))
-        .pipe(gulp.dest('./app/css'))
+gulp.task('images', function() {
+
+    gulp.src('app/img/**')
+        .pipe(plumber())
+        .pipe(size({title: 'Before optimization'}))
+        .pipe(cache(imgOptimization({ optimizationLevel: 5, progressive: true, interlaced: true})))
+        .pipe(size({title: 'After optimization'}))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('clear', function (done) {
+    return cache.clearAll(done);
+});
+
+gulp.task('getBowerJsFiles', function() {
+    return gulp.src(mainBowerFiles())
+        .pipe(gulpFilter('**/*.js'))
+        .pipe(gulp.dest('app/riba'));
 });
 
 gulp.task('concatCss', function() {
-    return gulp.src(['./dev/css/**'])
-        .pipe(concat('ui.min.css'))
-        //.pipe(prefix({
-        //    browsers: ['> 1%', 'last 2 versions', 'ie 8']
-        //}))
+    return gulp.src(['app/css/**'])
+        .pipe(plumber())
+        .pipe(size({showFiles: true}))
+        .pipe(concat('ui.css'))
+        .pipe(size({title: 'Cleaned css'}))
+        .pipe(size({title: 'Concatenated Css'}))
         .pipe(minify())
-        .pipe(gulp.dest('./app/css/ui'))
-        .pipe(connect.reload());
+        .pipe(rename({suffix: '.min'}))
+        .pipe(size({title: 'Minified Css'}))
+        .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('uncss', function() {
+    return gulp.src('dist/css/ui.min.css')
+        .pipe(plumber())
+        .pipe(uncss({
+            html: ['dist/index.html']
+        }))
+        .pipe(gulp.dest('dist/css/out'));
 });
 
 gulp.task('html', function() {
